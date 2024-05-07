a848fd9d5d772f9d4d16feb0489b9e903fe5e0ef
purvik6062 <purvikpanchal6336@gmail.com>
2024-05-07 11:59:09 +0530

    adding links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import icon from "../public/logo.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Forum | TroniQue",
   description: "",
+  icons: {
+    icon: ["../public/logo.png"],
+  },
 };
 
 export default function RootLayout({
